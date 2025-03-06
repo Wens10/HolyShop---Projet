f7eb19adb95c6592900419c615c39c05bb2de725
Wens10 <judebouity19@gmail.com>
2025-03-06 10:55:00 +0100

    create account

---
+++ responsive.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".hamburger-menu");
+    const navbar = document.querySelector(".navbar");
+
+    hamburger.addEventListener("click", () => {
+        navbar.classList.toggle("active");
+      
+    });
+});
